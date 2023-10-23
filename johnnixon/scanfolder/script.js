94222841ca1e23effea3f99f8d7265c4920ff8de
Luigigigi <luigizemeng@gmail.com>
2023-10-23 19:16:03 +1100

    1step adjust

--- johnnixon/scanfolder/script.js
+++ johnnixon/scanfolder/script.js
@@ -47,22 +47,6 @@
       btnPause.classList.remove("is-hidden");
     });
   
-    // pause
-    btnPause.addEventListener("click", function () {
-      video.pause();
-      btnPause.classList.add("is-hidden");
-      btnPlay.classList.remove("is-hidden");
-    });
-  
-    // take screenshot
-    btnScreenshot.addEventListener("click", function () {
-      const img = document.createElement("img");
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      canvas.getContext("2d").drawImage(video, 0, 0);
-      img.src = canvas.toDataURL("image/png");
-      screenshotsContainer.prepend(img);
-    });
   
     // switch camera
     btnChangeCamera.addEventListener("click", function () {
